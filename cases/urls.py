018ce0b98204b419c84193a27df9890aa4ac1158
Mushfiqur Rahman <rahmanmushfiqur719@gmail.com>
2023-07-08 12:44:05 +0600

    added todays,tomorrows case view, required html pages

--- cases/urls.py
+++ cases/urls.py
@@ -5,5 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.home , name='home'),
     path('create/',views.createCase, name='create-case'),
-    path('all-case/',views.getAllCases,name='all-case'),
+    path('all-cases/',views.getAllCases,name='all-cases'),
+    path('today-cases/',views.getAllCases,name='today-cases'),
+    path('tomorrow-cases/',views.getAllCases,name='tomorrow-cases'),
 ]
--- cases/views.py
+++ cases/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, redirect
 from .forms import CaseForm
 from .models import Case
+from datetime import date, timedelta
 
 # Create your views here.
 
@@ -20,14 +21,28 @@ def createCase(request):
     
     return render(request,'cases/create_case.html',{'form':form})
 
+# All Cases
 def getAllCases(request):
     cases = Case.objects.all()
     return render(request, 'cases/all_cases.html', {'cases': cases})
 
-# All Cases
-# Running Cases
 # Todays Cases
+def todays_case_list(request):
+    today = date.today()
+    cases = Case.objects.filter(date=today)
+    return render(request, 'cases/todays_cases.html', {'cases': cases})
+
 # Tomorrows Cases
+def tomorrows_case_list(request):
+    tomorrow = date.today() + timedelta(days=1)
+    cases = Case.objects.filter(date=tomorrow)
+    return render(request, 'cases/tomorrows_cases.html', {'cases': cases})
+
+
+
+# Running Cases
+
+
 # Decided Cases
 # Abandoned Cases
 # Not updated Cases
