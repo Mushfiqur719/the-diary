0a864ca964af04ac60002625ce0f14ea906f2aac
Mushfiqur Rahman <rahmanmushfiqur719@gmail.com>
2023-07-23 01:41:07 +0600

    added secret_key in settings.py

--- diary/settings.py
+++ diary/settings.py
@@ -15,17 +15,14 @@ from pathlib import Path
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
-from dotenv import load_dotenv
 import os
-
-load_dotenv()
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = '#1kst1=%+-)s=zfzaqi=1p$r!ycbjd1o2dr+&^t)#lk-z+r8x5'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
